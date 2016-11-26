00347b2602925d41494eae5aac1de164095878c1
ilmotta <icaro.ldm@gmail.com>
2016-11-26 19:27:40 -0200

    Experiment with basic namespacing rules

--- src/fwpd/core.clj
+++ src/fwpd/core.clj
@@ -1,4 +1,5 @@
 (ns fwpd.core
+  (:require [fwpd.visualization.svg :as svg])
   (:gen-class))
 
 (def filename "suspects.csv")
@@ -70,5 +71,27 @@
 
 (assert (= "Edward Cullen,10\nBella Swan,0" (to-csv (take 2 suspects))))
 
+(def heists [{:location "Cologne, Germany"
+              :cheese-name "Archbishop Hildebold's Cheese Pretzel"
+              :lat 50.95
+              :lng 6.97}
+             {:location "Zurich, Switzerland"
+              :cheese-name "The Standard Emmental"
+              :lat 47.37
+              :lng 8.55}
+             {:location "Marseille, France"
+              :cheese-name "Le Fromage de Cosquer"
+              :lat 43.30
+              :lng 5.37}
+             {:location "Zurich, Switzerland"
+              :cheese-name "The Lesser Emmental"
+              :lat 47.37
+              :lng 8.55}
+             {:location "Vatican City"
+              :cheese-name "The Cheese of Turin"
+              :lat 41.90
+              :lng 12.45}])
+
 (defn -main
-  [& args])
+  [& args]
+  (println (svg/points heists)))
