114b423140977c138ab2b731d486c7dac63ba82f
ilmotta <icaro.ldm@gmail.com>
2016-11-27 19:59:16 -0200

    Solve a few data structure exercises Gorilla REPL

--- src/brave/collections/unordered.clj
+++ src/brave/collections/unordered.clj
@@ -1,1 +1,15 @@
 (ns brave.collections.unordered)
+
+(defn next-round
+  [game]
+  (update-in game [:round] inc))
+
+(defn add-score
+  [game player amount]
+  (update-in game [:scores player] (fnil + 0) amount))
+
+(defn find-player
+  [game player-name]
+  (first (filter
+           #(= player-name (:name %))
+           (:players game))))
--- test/brave/collections/unordered_test.clj
+++ test/brave/collections/unordered_test.clj
@@ -37,3 +37,28 @@
 ; accessors
 (expect "Noob" (:name {:name "Noob"}))
 (expect "cat-with-default-name" (:name {:age 15} "cat-with-default-name"))
+
+(def game
+  {:round 2
+   :players #{{:name "Una" :ranking 43}
+              {:name "Bob" :ranking 77}
+              {:name "Cid" :ranking 33}}
+   :scores {"Una" 1400
+            "Bob" 1240
+            "Cid" 1024}})
+
+; Advance the game round
+(expect 3 (get (next-round game) :round))
+
+; Add score amount to the given player name
+(expect 1524 (get-in (add-score game "Cid" 500) [:scores "Cid"]))
+
+; Creates new player with given score if none is found
+(expect 100 (get-in (add-score game "Moke" 100) [:scores "Moke"]))
+
+; Find a player by name
+(expect {:name "Una" :ranking 43}
+        (find-player game "Una"))
+
+; Returns nil if player cannot be found
+(expect nil (find-player game "Moke"))
