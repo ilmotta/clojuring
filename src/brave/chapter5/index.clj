e502366ad9eed50520a58a69629c68b4635f3374
ilmotta <icaro.ldm@gmail.com>
2016-11-27 13:44:57 -0200

    Experiment with chapter 5 concepts, like comp

---
+++ src/brave/chapter5/index.clj
@@ -0,0 +1,42 @@
+(ns brave.chapter5.index)
+
+(def not-empty? (complement empty?))
+
+(defn print-each
+  [& values]
+  (when (not-empty? values)
+    (println (first values))
+    (recur (rest values))))
+
+(defn print-each-with-loop
+  [& values]
+  (loop [remaining-values values]
+    (when (not-empty? remaining-values)
+      (println (first remaining-values))
+      (recur (rest remaining-values)))))
+
+(defn pipe3
+  [f g h]
+  (fn [& args]
+    (h (g (apply f args)))))
+
+(defn pipen
+  [& fns]
+  (fn [& args]
+    (reduce #(%2 %1)
+            (apply (first fns) args)
+            (rest fns))))
+
+(defn compn
+  [& fns]
+  (fn [& args]
+    (apply (apply pipen (reverse fns)) args)))
+
+(defn mapset
+  [& args]
+  (set (apply map args)))
+
+(defn attr
+  "returns the characters attribute"
+  [property]
+  (comp property :attributes))
---
+++ test/brave/chapter5/index_test.clj
@@ -0,0 +1,29 @@
+(ns brave.chapter5.index-test
+  (:require [clojure.test :refer :all]
+            [brave.chapter5.index :refer :all]))
+
+(deftest fn-pipe3
+  (testing "pipes three function calls"
+    (is (= 9 ((pipe3 clojure.string/split first read-string) "9,..." #",")))))
+
+(deftest fn-pipen
+  (testing "pipes n-function calls"
+    (is (= 3 ((pipen #(+ 5 %) inc #(/ % 2) dec dec) 4)))))
+
+(deftest fn-compn
+  (testing "composes n-function calls"
+    (is (= 3 ((compn dec dec #(/ % 2) inc #(+ 5 %)) 4)))))
+
+(deftest fn-mapset
+  (testing "works just like map, but returns a set"
+    (is (= '#{2 3 4} (mapset inc [1 1 2 3 3])))))
+
+(deftest fn-attr
+  (testing "returns a characters attribute"
+    (is (= 10 ((attr :intelligence) {:attributes {:intelligence 10 :strength 5}})))))
+
+(deftest fn-not-empty
+  (testing "returns false if collection is empty"
+    (is (= false (not-empty? []))))
+  (testing "returns true if collection is not empty"
+    (is (= true (not-empty? [1])))))
